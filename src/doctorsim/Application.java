1e083c3b115367d4d6dbde6c2cbdd3dd24c05f18
Michael Rees <mrees791@gmail.com>
2021-11-17 11:26:32 -0500

    Initial commit.

---
+++ src/doctorsim/Application.java
@@ -0,0 +1,11 @@
+package doctorsim;
+
+public class Application {
+	public static void main(String[] args) {
+		new Application().start();
+	}
+	
+	public void start() {
+		System.out.println("Doctor Sim");
+	}
+}
